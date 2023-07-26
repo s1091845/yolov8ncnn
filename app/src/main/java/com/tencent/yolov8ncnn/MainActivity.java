c902ed27e7213390b037e0ea4ee8a71e9b3dc425
江俊頡 <s1091845@gm.pu.edu.tw>
2023-07-26 09:43:02 +0800

    fix: 修正前後鏡頭預設值

--- app/src/main/java/com/tencent/yolov8ncnn/MainActivity.java
+++ app/src/main/java/com/tencent/yolov8ncnn/MainActivity.java
@@ -37,7 +37,8 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback
     public static final int REQUEST_CAMERA = 100;
 
     private Yolov8Ncnn yolov8ncnn = new Yolov8Ncnn();
-    private int facing = 0;
+//    private int facing = 0;
+    private int facing = 1;
 
     private Spinner spinnerModel;
     private Spinner spinnerCPUGPU;
